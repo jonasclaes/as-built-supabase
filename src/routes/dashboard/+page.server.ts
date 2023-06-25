622a6468a52d93a3d50762a55331e4083fa8d28d
Jonas Claes <jonas@jonasclaes.be>
2023-06-25 15:14:07 +0200

    Added basic retrieval of projects for an organization

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,12 +1,23 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ locals: { getSession } }) => {
+export const load = (async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
 
 	if (!session) {
 		throw redirect(303, '/');
 	}
 
-	return { session };
+	const { data: profile } = await supabase
+		.from('profiles')
+		.select(`username, full_name, website, organization`)
+		.eq('id', session.user.id)
+		.single();
+
+	const { data: projects } = await supabase
+		.from('projects')
+		.select(`code, name`)
+		.eq('organization', profile?.organization);
+
+	return { session, projects };
 }) satisfies PageServerLoad;
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,8 +2,14 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let { projects } = data;
+	$: ({ projects } = data);
 </script>
 
 <section class="flex flex-col gap-3 w-full max-w-2xl mx-auto p-3">
 	<h1>Testy</h1>
+	{#each projects ?? [] as project}
+		<p><span>{project.code}</span>&nbsp;{project.name}</p>
+	{/each}
 </section>
