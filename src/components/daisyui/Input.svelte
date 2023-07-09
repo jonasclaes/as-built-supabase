363c57e83e1e83941e291d9ba08bfee0e484866e
Jonas Claes <jonas@jonasclaes.be>
2023-07-09 21:00:04 +0200

    Added Input component

---
+++ src/components/daisyui/Input.svelte
@@ -0,0 +1,92 @@
+<script lang="ts" context="module">
+	export type InputType =
+		| 'button'
+		| 'checkbox'
+		| 'color'
+		| 'date'
+		| 'datetime-local'
+		| 'email'
+		| 'file'
+		| 'hidden'
+		| 'image'
+		| 'month'
+		| 'number'
+		| 'password'
+		| 'radio'
+		| 'range'
+		| 'reset'
+		| 'search'
+		| 'submit'
+		| 'tel'
+		| 'text'
+		| 'time'
+		| 'url'
+		| 'week'
+		| (string & NonNullable<unknown>);
+	export type InputSize = 'xs' | 'sm' | 'md' | 'lg';
+</script>
+
+<script lang="ts">
+	import { applyClassIf } from '$lib/applyClassIf';
+
+	import { combineClasses } from '$lib/combineClasses';
+	import { prefixClass } from '$lib/prefix';
+
+	export let formControlClasses = '';
+	export let inputClasses = '';
+	export let name: string;
+	export let label: string | null = null;
+	export let type: InputType = 'text';
+	export let placeholder: string | null = null;
+	export let disabled: boolean | null = null;
+	export let required: boolean | null = null;
+	export let bordered = false;
+	export let ghost = false;
+	export let primary = false;
+	export let secondary = false;
+	export let accent = false;
+	export let info = false;
+	export let success = false;
+	export let warning = false;
+	export let error = false;
+	export let size: InputSize | null = null;
+	export let value: unknown = null;
+	export let helpText: string | null = null;
+</script>
+
+<div class={combineClasses('form-control', 'w-full', formControlClasses)}>
+	{#if label}
+		<label class="label" for={name}>
+			<span class="label-text">{label}</span>
+		</label>
+	{/if}
+	<input
+		{type}
+		{name}
+		{placeholder}
+		id={name}
+		class={combineClasses(
+			'input',
+			'w-full',
+			inputClasses,
+			applyClassIf(bordered, 'input-bordered'),
+			applyClassIf(ghost, 'input-ghost'),
+			applyClassIf(primary, 'input-primary'),
+			applyClassIf(secondary, 'input-secondary'),
+			applyClassIf(accent, 'input-accent'),
+			applyClassIf(info, 'input-info'),
+			applyClassIf(success, 'input-success'),
+			applyClassIf(warning, 'input-warning'),
+			applyClassIf(error, 'input-error'),
+			applyClassIf(size !== null, prefixClass('input-', size))
+		)}
+		{value}
+		{disabled}
+		{required}
+	/>
+	{#if helpText}
+		<label class="label" for={name}>
+			<span class="label-text-alt">{helpText}</span>
+		</label>
+	{/if}
+</div>
