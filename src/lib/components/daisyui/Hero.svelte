8b7f743e1621cac07c0e428a6151502507445804
Jonas Claes <jonas@jonasclaes.be>
2023-07-10 14:10:43 +0200

    Added error page and Hero component

---
+++ src/lib/components/daisyui/Hero.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let title: string;
+	export let body = '';
+	export let buttonText: string | null = null;
+	export let buttonHref = '#';
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">{title}</h1>
+			<p class="py-6">
+				{body}
+			</p>
+			{#if buttonText}
+				<a class="btn btn-primary" href={buttonHref}>{buttonText}</a>
+			{/if}
+		</div>
+	</div>
+</div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Hero from '$lib/components/daisyui/Hero.svelte';
+</script>
+
+<Hero
+	title={$page.status.toString()}
+	body={$page.error?.message}
+	buttonText="Go to dashboard"
+	buttonHref="/"
+/>
