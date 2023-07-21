473755724dc06b9827193e325527e05ef81199c5
Jonas Claes <jonas@jonasclaes.be>
2023-07-21 17:06:10 +0200

    Added fix for missing environment variables

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
 // src/hooks.server.ts
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.supabase = createSupabaseServerClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl: env.PUBLIC_SUPABASE_URL,
+		supabaseKey: env.PUBLIC_SUPABASE_ANON_KEY,
 		event
 	});
 
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { Database } from '../SupabaseDatabaseDefinitions';
 import type { LayoutLoad } from './$types';
@@ -7,8 +7,8 @@ export const load = (async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
 	const supabase = createSupabaseLoadClient<Database>({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl: env.PUBLIC_SUPABASE_URL,
+		supabaseKey: env.PUBLIC_SUPABASE_ANON_KEY,
 		event: { fetch },
 		serverSession: data.session
 	});
