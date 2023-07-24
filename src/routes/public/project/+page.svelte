05ee0e4c231be1318869c4c089292d4511f59677
Jonas Claes <jonas@jonasclaes.be>
2023-07-24 20:45:39 +0200

    Implemented fetching of data using Supabase service role

--- src/routes/public/project/+page.svelte
+++ src/routes/public/project/+page.svelte
@@ -2,16 +2,18 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let { token, payload, project, organization } = data;
+	$: ({ token, payload, project, organization } = data);
 </script>
 
 <section class="flex flex-col gap-3 w-full max-w-2xl mx-auto p-3">
 	<div>
-		<h1 class="text-3xl">Placeholder</h1>
-		<p class="text-base-content text-opacity-50">Public project</p>
+		<h1 class="text-3xl">{project.name}</h1>
+		<p class="text-base-content text-opacity-50">{organization.name}</p>
 	</div>
 	<p>Signature:</p>
-	<code class="break-all">{data.token}</code>
+	<code class="break-all">{token}</code>
 	<p>Decoded data:</p>
-	<textarea class="font-mono bg-base-200" rows="5">{JSON.stringify(data.payload, null, 2)}</textarea
-	>
+	<textarea class="font-mono bg-base-200" rows="5">{JSON.stringify(payload, null, 2)}</textarea>
 </section>
