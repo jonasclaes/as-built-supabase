83e4c89a67dd166bfe7b7bab09bd371c3e2664bf
Jonas Claes <jonas@jonasclaes.be>
2023-09-05 09:44:19 +0000

    add Stats components

---
+++ src/lib/components/daisyui/Stat.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { combineClasses } from '$lib/CombineClasses';
+
+	export let title: string | null = null;
+	export let value: string | null = null;
+	export let description: string | null = null;
+	export let figure = false;
+	export let actions = false;
+	export let _class = '';
+	export { _class as class };
+</script>
+
+<div class={combineClasses('stat', _class)} {...$$restProps}>
+	{#if figure}
+		<div class="stat-figure text-secondary">
+			<slot name="figure" />
+		</div>
+	{/if}
+	{#if title}
+		<div class="stat-title">{title}</div>
+	{/if}
+	{#if value}
+		<div class="stat-value">{value}</div>
+	{/if}
+	{#if description}
+		<div class="stat-desc">{description}</div>
+	{/if}
+	{#if actions}
+		<div class="stat-actions">
+			<slot name="actions" />
+		</div>
+	{/if}
+</div>
---
+++ src/lib/components/daisyui/Stats.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+	export type StatsOrientation = '' | 'horizontal' | 'vertical';
+</script>
+
+<script lang="ts">
+	import { combineClasses } from '$lib/CombineClasses';
+
+	const ORIENTATION_MAPS: Record<StatsOrientation, string> = {
+		'': '',
+		horizontal: 'stats-horizontal',
+		vertical: 'stats-vertical'
+	};
+
+	export let orientation: StatsOrientation = '';
+	export let _class = '';
+	export { _class as class };
+</script>
+
+<div class={combineClasses('stats', ORIENTATION_MAPS[orientation], _class)} {...$$restProps}>
+	<slot />
+</div>
