19d959fd5dd2e6ea2f256f6f19549dfd0b3b0b64
Jonas Claes <jonas@jonasclaes.be>
2023-09-05 09:44:10 +0000

    add Divider component

---
+++ src/lib/components/daisyui/Divider.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+	export type DividerOrientation = '' | 'horizontal' | 'vertical';
+</script>
+
+<script lang="ts">
+	import { combineClasses } from '$lib/CombineClasses';
+
+	const ORIENTATION_MAPS: Record<DividerOrientation, string> = {
+		'': '',
+		horizontal: 'divider-horizontal',
+		vertical: 'divider-vertical'
+	};
+
+	export let orientation: DividerOrientation = '';
+	export let _class = '';
+	export { _class as class };
+</script>
+
+<div class={combineClasses('divider', ORIENTATION_MAPS[orientation], _class)} {...$$restProps}>
+	<slot />
+</div>
