f9106c2e07584ef36aaa048582ab6207d3881584
Jonas Claes <jonas@jonasclaes.be>
2023-09-09 21:26:04 +0000

    add global error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Hero from '$lib/components/daisyui/Hero.svelte';
+	import Link from '$lib/components/daisyui/Link.svelte';
+</script>
+
+<Hero>
+	<h1 class="text-5xl font-bold">{$page.status}</h1>
+	<p class="py-6">
+		{$page.error?.message}
+	</p>
+	<Link href="/" color="primary" slot="actions">Go to home</Link>
+</Hero>
