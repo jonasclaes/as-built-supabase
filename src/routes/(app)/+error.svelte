cfd2fc13b644889f26869294a5ddd0a1afd524a8
Jonas Claes <jonas@jonasclaes.be>
2023-09-09 21:25:49 +0000

    improve Hero component

--- src/routes/(app)/+error.svelte
+++ src/routes/(app)/+error.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import Hero from '$lib/components/daisyui/Hero.svelte';
+	import Link from '$lib/components/daisyui/Link.svelte';
 </script>
 
-<Hero
-	title={$page.status.toString()}
-	body={$page.error?.message}
-	buttonText="Go to dashboard"
-	buttonHref="/"
-/>
+<Hero>
+	<h1 class="text-5xl font-bold">{$page.status}</h1>
+	<p class="py-6">
+		{$page.error?.message}
+	</p>
+	<Link href="/" color="primary" slot="actions">Go to dashboard</Link>
+</Hero>
--- src/routes/(public)/+error.svelte
+++ src/routes/(public)/+error.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import Hero from '$lib/components/daisyui/Hero.svelte';
+	import Link from '$lib/components/daisyui/Link.svelte';
+	import { publicStore } from '$lib/stores/publicStore';
 </script>
 
-<Hero title={$page.status.toString()} body={$page.error?.message} />
+<Hero>
+	<h1 class="text-5xl font-bold">{$page.status}</h1>
+	<p class="py-6">
+		{$page.error?.message}
+	</p>
+	<Link href="/public/project/{$publicStore.projectId}" color="primary" slot="actions"
+		>Go to project home</Link
+	>
+</Hero>
